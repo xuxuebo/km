5ae4fab58b7ece9f7232a31f0aed4324fabf0ca1
xuxuebo <xuxuebo@qgutech.com>
2018-08-23 17:13:23 +0800

    Privacy Violation: Heap Inspection 漏洞

--- km/src/main/java/com/qgutech/km/utils/KMBasicDataSource.java
+++ km/src/main/java/com/qgutech/km/utils/KMBasicDataSource.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 import sun.misc.BASE64Decoder;
 
 import java.sql.SQLFeatureNotSupportedException;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class KMBasicDataSource extends BasicDataSource {
@@ -25,7 +26,9 @@ public class KMBasicDataSource extends BasicDataSource {
             for (int i = 0; i < 5; i++) {
                 bytes = decoder.decodeBuffer(new String(bytes, "UTF-8"));
             }
-            return new String(bytes, "UTF-8");
+            String returnString=new String(bytes, "UTF-8");
+            Arrays.fill(bytes,(byte)0);
+            return returnString;
         } catch (Exception e) {
             e.printStackTrace();
         }
